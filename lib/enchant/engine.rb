5c876f5d610408f1c813370e50c127d35a90296d
Paolo Perego <thesp0nge@gmail.com>
2012-02-16 19:59:01 +0100

    Do some coding in scan method

--- lib/enchant/engine.rb
+++ lib/enchant/engine.rb
@@ -4,49 +4,73 @@ require 'uri'
 
 module Enchant
   class Engine
-    attr_reader :server, :code
-    attr_accessor :host, :port, :domain
+    attr_reader :urls_open
+    attr_reader :urls_internal_error
+    attr_reader :urls_private
 
     def initialize(options={})
       @host = options[:host]
       @port = options[:port]
       @wordlist = options[:wordlist]
+      @verbose = options[:verbose]
     end
 
+    
+    def fuzz(*) 
+      # in future some perturbation will be done here
+      get_list
+    end
+
+    def scan
+      http = Net::HTTP.new(host, port)
+      list = get_list
+
+      @urls_open={}
+      @urls_internal_error={}
+      @urls_private={}
+
+      list.each do |path|
+        begin
+          response = http.get(path)
+          c = response.code
+          if c == 200
+            @urls_open << path
+          end
+          if c == 401
+            @urls_private << path
+          end
+          if c >= 500
+            @urls_internal_error << path
+          end
+        rescue Net::HTTPBadResponse
+          if @verbose
+            puts "#{$!}".color(:red)
+          end
+        rescue Errno::ETIMEDOUT
+          if @verbose
+            puts "#{$!}".color(:red)
+          end
+        end
+
+      end
+    end
+
+    def up?
+      Net::HTTP.new(@host, @port).head('/').kind_of? Net::HTTPOK
+    end  
+
+
+    private
     def get_list
       begin
         File.open(@wordlist, 'r') { |f|
           @list = f.readlines
         }
       rescue Errno::ENOENT
-        puts "It seems the wordlist file is not present (#{wordlist})"
+        puts "it seems the wordlist file is not present (#{wordlist})".color(:red)
         @list = nil
       end
     end
-
-    def fuzz(*) 
-      # in future some perturbation will be done here
-      @list
-    end
-
-    def scan
-      http = Net::HTTP.new(host, port)
-      begin
-        response = http.get(path)
-        @code = response.code
-      rescue Net::HTTPBadResponse
-        puts #{$!}
-        @code=-1
-      rescue Errno::ETIMEDOUT
-        puts #{$!}
-        @code=-1
-      end
-      @code
-    end
-
-    def up?(site)
-      Net::HTTP.new(site).head('/').kind_of? Net::HTTPOK
-    end  
 
   end
 end
